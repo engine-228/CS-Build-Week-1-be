d4e094caf434e2a9e52670ab26e3af59ec45226e
Melissa Murphy <melissa@thoughtfulmonkey.design>
2020-03-04 20:38:48 -0700

    connecting rooms to api

--- theundefinedbackend/urls.py
+++ theundefinedbackend/urls.py
@@ -17,14 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from api import views
+from undefined_world.views import RoomViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'rooms', RoomViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-    path('rooms/', include(undefined_world.urls))
+    # path('rooms/', include(undefined_world.urls))
 ]
