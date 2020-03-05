7a3051e571273914cacb63667cc69b6d42796728
Melissa Murphy <melissa@thoughtfulmonkey.design>
2020-03-05 11:16:39 -0700

    ready to run auth migrations

--- theundefinedbackend/settings.py
+++ theundefinedbackend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'rest_framework.authtoken',
     'undefined_world',
 ]
 
@@ -76,6 +77,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'theundefinedbackend.wsgi.application'
 
+# REST framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', )
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
--- theundefinedbackend/urls.py
+++ theundefinedbackend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from api import views
+from api.views import login
 from undefined_world.views import RoomViewSet
 
 router = routers.DefaultRouter()
@@ -26,7 +27,7 @@ router.register(r'rooms', RoomViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/login', login),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     # path('rooms/', include(undefined_world.urls))
 ]
